33e3ed7f94635dc84735eeab0aeb49d47d6ee9df
LonelyTower <sellsword.entertainment@gmail.com>
2016-04-16 14:28:57 +1000

    Initial Commit & Project Setup

---
+++ Assets/Movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+
+    public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.down*speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.up * speed);
+        }
+    }
+}
